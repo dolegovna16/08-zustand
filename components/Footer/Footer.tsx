334950dc98dc1a612152d4ebe9f5cb4667b01708
Daria Chaiun <dariachiun@gmail.com>
2025-11-15 21:28:23 +0200

    1

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,7 +7,6 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Oleksiy Kavderko</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app"> student@notehub.app</a>
